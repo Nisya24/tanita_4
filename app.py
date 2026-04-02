fbac75882684b09a4c0cc8a7a2942ce2d512df45
Nisya24 <nisyaramadhani2005@mail.ugm.ac.id>
2026-04-03 00:54:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
-# BACKGROUND PINK
+# BACKGROUND + FONT COLOR
 st.markdown(
     """
     <style>
     .stApp {
         background-color: #FFC2D1;
+    }
+
+    /* semua teks */
+    html, body, [class*="css"] {
+        color: #31333f;
     }
     </style>
     """,
@@ -25,12 +30,12 @@ if not st.session_state.start:
             st.session_state.start = True
             st.rerun()
 
-# setelah klik → code asli lo
+# setelah klik → code asli
 else:
     st.title('HAPPY BIRTHDAY TANITA')
     st.write('Selamat hari lahir ya untuk my teman sahabat dari janin my forever blackpink member. Selamat sdh memulai hidup baru di tahun ke 20 ini, sering-sering ketemuan ya kita. Love you from melbin <3')
 
-    slider_value = st.slider('rating web buatan gweh plis', min_value=0, max_value=100, value=50)
+    slider_value = st.slider('rating seberapa bahagia diri lo', min_value=0, max_value=100, value=50)
 
     if (slider_value > 51):
         VIDEO_URL = "https://youtu.be/NndwCpJVurc?si=6eWc5Dy9C7JPARX5"
