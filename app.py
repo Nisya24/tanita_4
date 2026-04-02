182b0b0b731b9c3c25b8086e5617c8a27df61313
Nisya24 <nisyaramadhani2005@mail.ugm.ac.id>
2026-04-03 00:48:37 +0700

    Fix typo in slider label in app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ else:
     st.title('HAPPY BIRTHDAY TANITA')
     st.write('Selamat hari lahir ya untuk my teman sahabat dari janin my forever blackpink member. Selamat sdh memulai hidup baru di tahun ke 20 ini, sering-sering ketemuan ya kita. Love you from melbin <3')
 
-    slider_value = st.slider('rating web buatan gweh plis)', min_value=0, max_value=100, value=50)
+    slider_value = st.slider('rating web buatan gweh plis', min_value=0, max_value=100, value=50)
 
     if (slider_value > 51):
         VIDEO_URL = "https://youtu.be/NndwCpJVurc?si=6eWc5Dy9C7JPARX5"
